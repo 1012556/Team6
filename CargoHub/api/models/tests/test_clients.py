b547894c2fbb244aacc2dcb8bc49e616e35bc950
DamianVirgilio <160112008+DamianVirgilio@users.noreply.github.com>
2024-10-02 10:36:46 +0200

    test

---
+++ CargoHub/api/models/tests/test_clients.py
@@ -0,0 +1,66 @@
+from models.clients import Clients
+
+client1 = {
+        "id": 1,
+        "name": "Damian Virgilio",
+        "address": "1296 Daniel Road Apt. 349",
+        "city": "Pierceview",
+        "zip_code": "28301",
+        "province": "Colorado",
+        "country": "United States",
+        "contact_name": "Bryan Clark",
+        "contact_phone": "242.732.3483x2573",
+        "contact_email": "robertcharles@example.net",
+        "created_at": "2010-04-28 02:22:53",
+        "updated_at": "2022-02-09 20:22:35"
+    }
+
+client2 = {
+        "id": 2,
+        "name": "Mees",
+        "address": "1296 Daniel Road Apt. 349",
+        "city": "Pierceview",
+        "zip_code": "28301",
+        "province": "Colorado",
+        "country": "United States",
+        "contact_name": "Bryan Clark",
+        "contact_phone": "242.732.3483x2573",
+        "contact_email": "robertcharles@example.net",
+        "created_at": "2010-04-28 02:22:53",
+        "updated_at": "2022-02-09 20:22:35"
+    }
+
+client3 = {
+        "id": 3,
+        "name": "Joey",
+        "address": "1296 Daniel Road Apt. 349",
+        "city": "Pierceview",
+        "zip_code": "28301",
+        "province": "Colorado",
+        "country": "United States",
+        "contact_name": "Bryan Clark",
+        "contact_phone": "242.732.3483x2573",
+        "contact_email": "robertcharles@example.net",
+        "created_at": "2010-04-28 02:22:53",
+        "updated_at": "2022-02-09 20:22:35"
+    }
+
+client4 = {
+        "id": 4,
+        "name": "Joey",
+        "address": "1296 Daniel Road Apt. 349",
+        "city": "Pierceview",
+        "zip_code": "28301",
+        "province": "Colorado",
+        "country": "United States",
+        "contact_name": "Bryan Clark",
+        "contact_phone": "242.732.3483x2573",
+        "contact_email": "robertcharles@example.net",
+        "created_at": "2010-04-28 02:22:53",
+        "updated_at": "2022-02-09 20:22:35"
+    }
+
+def test_AddClients():
+    obj = Clients()
+    obj.Add_client(client1)
+    assert len(obj.CLIENTS) == 1
